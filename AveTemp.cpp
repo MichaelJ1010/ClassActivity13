7c6904d928944920a724be135a00e6f1d2f7f880
Michael J <30031129@cc.peralta.edu>
2024-09-17 18:25:11 -0700

    created list of temperatures

--- AveTemp.cpp
+++ AveTemp.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 
 int main(){
+    double totalTemperature = 0;
+    double temperature;
+    int numberOfTemperatures;
+    
+    cout << "How many temperatures do you want to add?" << endl; 
 
+    cin >> numberOfTemperatures;
+    int temperatures[numberOfTemperatures];
+    
     return 0;
 }
